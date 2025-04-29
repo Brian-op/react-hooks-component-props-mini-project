34422351b798a82aed9603353fdcb9af0a54b0de
Brian <brian.kiprono8@student.moringaschool.com>
2025-04-29 23:21:28 +0300

    last try

--- src/__tests__/About.test.js
+++ src/__tests__/About.test.js
@@ -1,7 +1,8 @@
 import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
 import About from "../components/About";
-import logo from "../assets/logo"
+
+const logo = "https://via.placeholder.com/215";
 
 test("renders a <aside> element", () => {
   const { container } = render(<About />);
